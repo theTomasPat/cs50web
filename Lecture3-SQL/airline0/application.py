ab4870735efc7cc86a4b1c9256a4f84ccab0dd6d
Tomas P <tomas.patlan.1@gmail.com>
2018-08-01 17:08:35 -0300

    Began airline0 setup

---
+++ Lecture3-SQL/airline0/application.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+@app.route("/")
+def index():
+  flights = db.execute("SELECT * FROM flights").fetchall()
+  return render_template("index.html", flights=flights)
+
+#@app.route("/book", methods={"POST"})
+#def book():
