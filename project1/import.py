37531c868add9c8ff5d4236fb073fbacf634049f
Tomas P <tomas.patlan.1@gmail.com>
2018-08-02 18:50:40 -0300

    Added import.py

---
+++ project1/import.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+""" Read the rows from the given file and add them to the DB """
+""" Assume the first row in the CSV is a header """
+def main(fileToImport):
+  with open(fileToImport) as infile:
+    csvReader = csv.reader(infile, dialect='excel')
+    next(csvReader, None)
+
+    for entry in csvReader:
+      db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {
+        "isbn": entry[0],
+        "title": entry[1],
+        "author": entry[2],
+        "year": entry[3]
+        })
+      print(f"Adding {entry[1]} by {entry[2]}")
+    db.commit()
+    print("Import complete")
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    raise ValueError("A file must be given to import from")
+
+  main(sys.argv[1])
