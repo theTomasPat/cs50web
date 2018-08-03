5c8ebc11d6b0a029c5a654f8ef60d6afb717400e
Tomas P <tomas.patlan.1@gmail.com>
2018-08-02 23:38:58 -0300

    Began adding support for GoodReads helper method

--- project1/import.py
+++ project1/import.py
@@ -5,6 +5,8 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
+import GoodReads
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
@@ -12,15 +14,21 @@ db = scoped_session(sessionmaker(bind=engine))
 """ Assume the first row in the CSV is a header """
 def main(fileToImport):
   with open(fileToImport) as infile:
-    csvReader = csv.reader(infile, dialect='excel')
+    csvReader = csv.reader(infile)
     next(csvReader, None)
 
     for entry in csvReader:
-      db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {
+      # TODO: retrieve book info from GoodReads using ISBN
+      bookInfo = GoodReads.bookInfoISBN(entry[0])
+
+      db.execute("INSERT INTO books (isbn, title, author, year, average_score, review_count, image_url) VALUES (:isbn, :title, :author, :year, :avgScore, :reviewCount, :imgURL)", {
         "isbn": entry[0],
         "title": entry[1],
         "author": entry[2],
-        "year": entry[3]
+        "year": entry[3],
+        "avgScore": bookInfo['average_score'],
+        "reviewCount": 0,
+        "imgURL": bookInfo['image_url']
         })
       print(f"Adding {entry[1]} by {entry[2]}")
     db.commit()
