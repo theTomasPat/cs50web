edc464ec7411183457bc332677acec6ab35241d7
Tomas P <tomas.patlan.1@gmail.com>
2018-08-05 01:47:00 -0300

    Added simple login example

--- project1/application.py
+++ project1/application.py
@@ -1,15 +1,16 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, request, redirect, escape, url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+  raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -23,4 +24,23 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+  if 'username' in session:
+    return "Logged in as {}".format(escape(session['username']))
+  return "You are not logged in."
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  if request.method == 'POST':
+    session['username'] = request.form['username']
+    return redirect(url_for('index'))
+  return '''
+    <form method="post">
+      <input type="text" name="username">
+      <button>Login</button>
+    </form>
+    '''
+
+@app.route("/logout")
+def logout():
+  session.pop('username', None)
+  return redirect(url_for('index'))
