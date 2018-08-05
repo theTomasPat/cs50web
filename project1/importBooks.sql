2eb28c1ba5b5eb19735a682c0f7350b9f662bd9c
Tomas P <tomas.patlan.1@gmail.com>
2018-08-05 00:29:50 -0300

    Updated table schema

--- project1/importBooks.sql
+++ project1/importBooks.sql
@@ -3,8 +3,5 @@ CREATE TABLE books (
   isbn VARCHAR NOT NULL,
   title VARCHAR NOT NULL,
   author VARCHAR NOT NULL,
-  year INTEGER NOT NULL,
-  average_score NUMERIC(3, 2),
-  review_count INTEGER,
-  image_url VARCHAR
+  year INTEGER NOT NULL
 );
