4a955d75bf01f742643fa2fd138108b5e5071308
Tomas P <tomas.patlan.1@gmail.com>
2018-08-09 22:55:41 -0300

    Implemented DB queries for username and email verification

--- project1/src/dbRunner.py
+++ project1/src/dbRunner.py
@@ -14,11 +14,11 @@ def addUser(db, username, email, password):
   Exception -- custom message depending on error
   """
   
-  if userExists(username):
-    raise Exception("Username is already taken")
+  if userExists(db, username):
+    raise Exception("Username is already taken!")
   
-  if emailExists(email):
-    raise Exception("There is already an account associated with that email")
+  if emailExists(db, email):
+    raise Exception("There is already an account associated with that email.")
 
   '''if not validPassword(password):
     raise Exception("Please enter a valid password")'''
@@ -34,20 +34,31 @@ def addUser(db, username, email, password):
   )
   db.commit()
 
-def userExists(username):
+def userExists(db, username):
   """
   Query the DB and return True if the username already exists, otherwise False
 
   Arguments:
   username -- String, the username to query the DB with
+
+  Returns:
+  Boolean -- whether or not the username already exists
   """
+  dbResponse = db.execute("SELECT username FROM users WHERE username = :username", {"username": username}).fetchone()
+
+  if dbResponse == None:
+    return False
   return True
 
-def emailExists(email):
+def emailExists(db, email):
   """
   Query the DB and return True if the email exists, otherwise False
 
   Arguments:
     email -- String, the email to query the DB with
   """
+  dbResponse = db.execute("SELECT email FROM users WHERE email = :email", {"email": email}).fetchone()
+
+  if dbResponse == None:
+    return False
   return True
