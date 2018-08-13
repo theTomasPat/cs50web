3b7d86b915aa5953a3bdd76421b34466acf2354e
Tomas P <tomas.patlan.1@gmail.com>
2018-08-12 22:54:17 -0300

    Updated reference to GoodReads module

--- project1/import.py
+++ project1/import.py
@@ -5,7 +5,7 @@ import csv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-import GoodReads
+import src.GoodReads
 
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
