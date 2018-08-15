78179c84c2305589d389cd70bdebecae2b6b75f6
Tomas P <tomas.patlan.1@gmail.com>
2018-08-14 23:57:30 -0300

    Added script to set env variables

---
+++ project1/commandLineSetup.sh
@@ -0,0 +1,5 @@
+# run with . commandLineSetup.sh
+
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
+export DATABASE_URL='postgres://uyihpsfrylvqyp:6d91a86b90fac41f5021d5e6db1eccdcdc772e815ec7a1cde1a0ad0d764d320e@ec2-54-204-21-226.compute-1.amazonaws.com:5432/daepvf7l2av0vc'
